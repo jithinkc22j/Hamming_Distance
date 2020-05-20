ef236f3d644774f8729a229140f2197aae72ac4d
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 13:28:31 +0530

    Add files via upload

---
+++ main.m
@@ -0,0 +1,28 @@
+% Main fuction - Hamming distance calculating function
+%-------------------------------------------------------------------------
+%--------------------------------------------------------------------------------------------------------------------
+% Note: If you are using my code for your system or project, you should always cite my paper as a reference.
+% K C.Jithin, SyamSankar, "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set",
+% Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
+%---------------------------------------------------------------------------------------------------------------------
+
+% DEMO:
+% d=123;
+% c=145;
+%-------------------------------------------------------------------------
+ %  Input your interger values
+ d= ;
+ c= ;
+%-------------------------------------------------------------------------
+ % Calling hamming distance calculation function
+ 
+  out=ham_dist(d,c);
+%-------------------------------------------------------------------------
+ % Output
+%-------------------------------------------------------------------------
+
+fprintf('Hamming distance of "%d" and "%d" : %d \n',d,c,out);
+
+%-------------------------------------------------------------------------
+ 
+ 
